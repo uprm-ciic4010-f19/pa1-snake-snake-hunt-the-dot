e6f54ab0441acd5d9fa11b20ebf5ba09fc46891d
Jesus843 <jesus.rodriguez2@upr.edu>
2019-09-05 13:52:25 -0400

    Added N key for add tail.

--- src/Input/KeyManager.java
+++ src/Input/KeyManager.java
@@ -12,7 +12,7 @@ public class KeyManager implements KeyListener {
 
 	private boolean[] keys,justPressed,cantPress;
 	public boolean up=false, down=false, left=false, right=false;
-	public boolean pbutt=false;
+	public boolean pbutt=false, nbutt=false;
 
 
 	public KeyManager(){
@@ -43,6 +43,7 @@ public class KeyManager implements KeyListener {
 		right = keys[KeyEvent.VK_D];
 
 		pbutt = keys[KeyEvent.VK_ESCAPE];
+		nbutt = keys[KeyEvent.VK_N];
 
 	}
 
